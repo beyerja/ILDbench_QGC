268f3224caf0c3152b08f290d1f77facd27312ca
Jakob Beyer <beyerjac@flc02.desy.de>
2019-01-10 09:33:35 +0100

    Add V_m plot for aQGC samples

--- macros/ROOT_macros/aQGCTestMacro.cpp
+++ macros/ROOT_macros/aQGCTestMacro.cpp
@@ -1,5 +1,5 @@
-#include "analyzers/src/EFTTester.cpp"
-// #include "analyzers/src/aQGCSampleSMAnalyzer.cpp"
+// #include "analyzers/src/EFTTester.cpp"
+#include "analyzers/src/aQGCSampleSMAnalyzer.cpp"
 
 void aQGCTestMacro() {
   EnableImplicitMT(20); // Allow multithreating in RDataFrame
@@ -42,19 +42,19 @@ void aQGCTestMacro() {
   float p_beam_polarization = 0.3;
   float luminosity = 1000;
   
-  string output_directory = "/afs/desy.de/group/flc/pool/beyerjac/VBS/aQGCAnalysis/analyzer_plots/EFTTester";
-  EFTTester analyzer;  
-  analyzer.setInputPaths( root_file_paths );
-  analyzer.setBeamPolarizations( e_beam_polarization, p_beam_polarization );
-  analyzer.setLuminosity( luminosity );
-  analyzer.setOutputDirectory( output_directory );
-  analyzer.run();
-  
-  // string output_directory = "/afs/desy.de/group/flc/pool/beyerjac/VBS/aQGCAnalysis/analyzer_plots/aQGCSampleSMAnalyzer";
-  // aQGCSampleSMAnalyzer analyzer;  
+  // string output_directory = "/afs/desy.de/group/flc/pool/beyerjac/VBS/aQGCAnalysis/analyzer_plots/EFTTester";
+  // EFTTester analyzer;  
   // analyzer.setInputPaths( root_file_paths );
   // analyzer.setBeamPolarizations( e_beam_polarization, p_beam_polarization );
   // analyzer.setLuminosity( luminosity );
   // analyzer.setOutputDirectory( output_directory );
   // analyzer.run();
+  
+  string output_directory = "/afs/desy.de/group/flc/pool/beyerjac/VBS/aQGCAnalysis/analyzer_plots/aQGCSampleSMAnalyzer";
+  aQGCSampleSMAnalyzer analyzer;  
+  analyzer.setInputPaths( root_file_paths );
+  analyzer.setBeamPolarizations( e_beam_polarization, p_beam_polarization );
+  analyzer.setLuminosity( luminosity );
+  analyzer.setOutputDirectory( output_directory );
+  analyzer.run();
 };
--- macros/ROOT_macros/analyzers/src/aQGCSampleSMAnalyzer.cpp
+++ macros/ROOT_macros/analyzers/src/aQGCSampleSMAnalyzer.cpp
@@ -13,9 +13,14 @@ void aQGCSampleSMAnalyzer::performAnalysis(){
   auto rdf_WWsignal = rdf_with_process_weight.Filter( IsTrueWWSignalCut, {"mctruth.signal_type"} );
   auto rdf_ZZsignal = rdf_with_process_weight.Filter( IsTrueZZSignalCut, {"mctruth.signal_type"} );
   
-  auto h1_VV_m = m_dataframe->Histo1D({"h1_VV_m", "Di-boson mass; m_{VV}; Events", 50, 0, 1000}, "mctruth.VV_m");
+  auto h1_VV_m = rdf_with_process_weight.Histo1D({"h1_VV_m", "Di-boson mass; m_{VV}; Events", 50, 0, 1000}, "mctruth.VV_m", "process_weight");
   auto h1_VV_m_WWsignal = rdf_WWsignal.Histo1D({"h1_VV_m_WWsignal", "Di-boson mass; m_{VV}; Events", 100, 0, 1000}, "mctruth.VV_m", "process_weight");
   auto h1_VV_m_ZZsignal = rdf_ZZsignal.Histo1D({"h1_VV_m_ZZsignal", "Di-boson mass; m_{VV}; Events", 100, 0, 1000}, "mctruth.VV_m", "process_weight");
+
+  auto h1_V1_m_WWsignal = rdf_WWsignal.Histo1D({"h1_V1_m_WWsignal", "Boson mass, WW like events; m_{V1}; Events", 75, 0, 150}, "mctruth.V1_m", "process_weight");
+  auto h1_V2_m_WWsignal = rdf_WWsignal.Histo1D({"h1_V2_m_WWsignal", "Boson mass, WW like events; m_{V2}; Events", 75, 0, 150}, "mctruth.V2_m", "process_weight");
+  auto h1_V1_m_ZZsignal = rdf_ZZsignal.Histo1D({"h1_V1_m_ZZsignal", "Boson mass, ZZ like events; m_{V1}; Events", 75, 0, 150}, "mctruth.V1_m", "process_weight");
+  auto h1_V2_m_ZZsignal = rdf_ZZsignal.Histo1D({"h1_V2_m_ZZsignal", "Boson mass, ZZ like events; m_{V2}; Events", 75, 0, 150}, "mctruth.V2_m", "process_weight");
 
   unique_ptr<TCanvas> canvas_VV_m (new TCanvas("VV_m", "", 0, 0, 600, 600));
   unique_ptr<THStack> VV_m_stack (new THStack("VV_m", "; m_{VV}; Events")); 
@@ -37,4 +42,29 @@ void aQGCSampleSMAnalyzer::performAnalysis(){
   h1_VV_m_WWsignal->Write();
   h1_VV_m_ZZsignal->Write();
   VV_m_file->Close();
+
+  unique_ptr<TCanvas> canvas_V_m (new TCanvas("V_m", "", 0, 0, 600, 600));
+  unique_ptr<THStack> V_m_stack (new THStack("V_m", "; m_{V}; 2*Events")); 
+  unique_ptr<TLegend> V_m_leg (new TLegend( 0.6, 0.6, 0.9, 0.9 )); 
+  auto h1_V_m_WWsignal = new TH1D( "h1_V_m_WWsignal", "Boson mass, WW like events; m_{V}; 2*Events", 75, 0, 150 );
+  auto h1_V_m_ZZsignal = new TH1D( "h1_V_m_ZZsignal", "Boson mass, ZZ like events; m_{V}; 2*Events", 75, 0, 150 );
+  h1_V_m_WWsignal->Add( h1_V1_m_WWsignal.GetPtr(), h1_V2_m_WWsignal.GetPtr() );
+  h1_V_m_ZZsignal->Add( h1_V1_m_ZZsignal.GetPtr(), h1_V2_m_ZZsignal.GetPtr() );
+  h1_V_m_WWsignal->SetLineColor(4);
+  h1_V_m_ZZsignal->SetLineColor(2);
+  h1_V_m_WWsignal->SetLineStyle(1);
+  h1_V_m_ZZsignal->SetLineStyle(1);
+  V_m_stack->Add( h1_V_m_WWsignal );
+  V_m_stack->Add( h1_V_m_ZZsignal );
+  V_m_leg->SetHeader("generator level");
+  V_m_leg->AddEntry( h1_V_m_WWsignal, "WW", "l" );
+  V_m_leg->AddEntry( h1_V_m_ZZsignal, "ZZ", "l" );
+  V_m_stack->Draw("nostack hist");
+  V_m_stack->GetYaxis()->SetMaxDigits(3);
+  V_m_leg->Draw();
+  canvas_V_m->Print( (this->getOutputDirectory() + "/V_m.pdf").c_str() );
+  unique_ptr<TFile> V_m_file ( new TFile( ( this->getOutputDirectory() + "/V_m.root").c_str(), "RECREATE" ) );
+  h1_V_m_WWsignal->Write();
+  h1_V_m_ZZsignal->Write();
+  V_m_file->Close();
 }
