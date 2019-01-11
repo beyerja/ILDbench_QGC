db3a635fa10250e3ad5b07db4b4eb1ad85eb0aaf
Jakob Beyer <beyerjac@flc02.desy.de>
2019-01-11 10:21:38 +0100

    Adjust to new WHIZARD2 version
    Adjust WHIZARD macros and scripts to new version of WHIZARD2 in which
    ISR+rescanning is possible.
    - In the scripts the masses of the quarks must now be explicitely set to
    0 (in order to use flavour summation).
    - In the macros the new gcc compiler version must be added to PATH so
    that WHIZARD can compile.

--- macros/WHIZARD_macros/run_single_rescan.sh
+++ macros/WHIZARD_macros/run_single_rescan.sh
@@ -17,6 +17,10 @@ if [[ ! -d ${condor_output_directory} ]] ; then # Create if not existing
   mkdir ${condor_output_directory}
 fi
 
+# TODO Set this centrally!
+# Need newer compiler version (same as WHIZARD uses) -> WARNING May need to be updated in new WHIZARD version!
+gcc_path="/nfs/dust/ilc/user/berggren/whiz2-tests/gcc6.3/x86_64-slc6/bin" 
+
 # Get input and split into directory and filename
 rescan_file_path=$1
 rescan_file_dir=$( cd "$( dirname "${rescan_file_path}"  )" && pwd  )
@@ -24,4 +28,4 @@ rescan_file_name=$( basename "${rescan_file_path}"  )
 
 # Submit job to condor 
 cd ${standard_condor_directory}
-condor_submit ${standard_condor_submit} arguments="cd ${rescan_file_dir} \&\& whizard ${rescan_file_name}"
+condor_submit ${standard_condor_submit} arguments="export PATH=${gcc_path}:\${PATH} \&\& cd ${rescan_file_dir} \&\& whizard ${rescan_file_name}"
