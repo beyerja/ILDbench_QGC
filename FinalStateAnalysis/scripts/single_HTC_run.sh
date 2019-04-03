8a00eab2c976c7c57c46e142459fe7271f4cb335
beyerja <beyerja@gmx.de>
2019-04-03 19:10:18 +0200

    Add new processor to MARLIN_DLL
    - Add new processor to MARLIN_DLL on BIRD

--- FinalStateAnalysis/scripts/single_HTC_run.sh
+++ FinalStateAnalysis/scripts/single_HTC_run.sh
@@ -6,6 +6,7 @@ VBSdir=/afs/desy.de/user/b/beyerjac/flc/VBS/nunu_hadrons
 PROJECTNAME=JakobsVBSProcessor
 libname=$VBSdir"/lib/lib$PROJECTNAME"
 export MARLIN_DLL=${MARLIN_DLL}:${libname}.so
+export MARLIN_DLL=${MARLIN_DLL}:${VBSdir}/TJJetRecoParticleFinder/lib/libTJJetRecoParticleFinder.so
 
 # For now use own TJ for better reco to jets
 for possible_lib in $( ls /cvmfs/ilc.desy.de/sw/x*_*_gcc*_sl*/v*/MarlinReco/v*/lib/libMarlinReco.so ); do
