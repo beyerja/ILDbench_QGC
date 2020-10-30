d017b42f6ccf32a47f614891a3b4554065c074db
Jakob Beyer <beyerja@gmx.de>
2020-10-30 19:01:51 +0100

    More uniform default separation fit settings
    - Make the default WWZZ separation fit settings more uniform.

--- FinalStateAnalysis/analysis/comparisons/comparison_separations/DefaultFitSettings.py
+++ FinalStateAnalysis/analysis/comparisons/comparison_separations/DefaultFitSettings.py
@@ -8,30 +8,30 @@ default_settings = {
       { 
         "WW":
           FS.FitSetting(SC.DOUBLE_GAUSSIAN, 
-            {"f1" : 0.7,
-            "f_m1" : 1.1,
-            "f_m2" : 0.9,
+            {"f1" : 0.8,
+            "f_m1" : 1.0,
+            "f_m2" : 1.0,
             "f_s1" : 0.5,
-            "f_s2" : 2.0
+            "f_s2" : 1.5
           }), 
         "ZZ":
           FS.FitSetting(SC.DOUBLE_GAUSSIAN, 
-            {"f1" : 0.7,
-            "f_m1" : 1.1,
-            "f_m2" : 0.9,
+            {"f1" : 0.8,
+            "f_m1" : 1.0,
+            "f_m2" : 1.0,
             "f_s1" : 0.5,
-            "f_s2" : 2.0
+            "f_s2" : 1.5
           })
       },
     "oc": 
       { 
         "WW":
           FS.FitSetting(SC.DOUBLE_GAUSSIAN, 
-            {"f1" : 0.7,
-            "f_m1" : 1.1,
-            "f_m2" : 0.9,
+            {"f1" : 0.8,
+            "f_m1" : 1.0,
+            "f_m2" : 1.0,
             "f_s1" : 0.5,
-            "f_s2" : 2.0
+            "f_s2" : 1.5
           }), 
         "ZZ":
           FS.FitSetting(SC.DOUBLE_GAUSSIAN, 
@@ -92,30 +92,30 @@ default_settings = {
           }), 
         "ZZ":
           FS.FitSetting(SC.DOUBLE_GAUSSIAN, 
-            {"f1" : 0.7,
+            {"f1" : 0.8,
             "f_m1" : 1.0,
             "f_m2" : 1.0,
-            "f_s1" : 0.6,
-            "f_s2" : 1.3
+            "f_s1" : 0.5,
+            "f_s2" : 1.5
           })
       },
     "gen": 
       { 
         "WW":
           FS.FitSetting(SC.DOUBLE_GAUSSIAN, 
-            {"f1" : 0.7,
+            {"f1" : 0.8,
             "f_m1" : 1.0,
-            "f_m2" : 0.98,
-            "f_s1" : 0.9,
-            "f_s2" : 1.2
+            "f_m2" : 1.0,
+            "f_s1" : 0.5,
+            "f_s2" : 1.5
           }), 
         "ZZ":
           FS.FitSetting(SC.DOUBLE_GAUSSIAN, 
-            {"f1" : 0.7,
+            {"f1" : 0.8,
             "f_m1" : 1.0,
             "f_m2" : 1.0,
-            "f_s1" : 0.9,
-            "f_s2" : 1.3
+            "f_s1" : 0.5,
+            "f_s2" : 1.5
           })
       }
     },
@@ -125,38 +125,38 @@ default_settings = {
       { 
         "WW":
           FS.FitSetting(SC.DOUBLE_GAUSSIAN, 
-            {"f1" : 0.7,
-            "f_m1" : 1.1,
-            "f_m2" : 0.9,
+            {"f1" : 0.8,
+            "f_m1" : 1.0,
+            "f_m2" : 1.0,
             "f_s1" : 0.5,
-            "f_s2" : 2.0
+            "f_s2" : 1.5
           }), 
         "ZZ":
           FS.FitSetting(SC.DOUBLE_GAUSSIAN, 
             {"f1" : 0.8,
             "f_m1" : 1.0,
-            "f_m2" : 0.9,
+            "f_m2" : 1.0,
             "f_s1" : 0.5,
-            "f_s2" : 2.0
+            "f_s2" : 1.5
           })
       },
     "oc": 
       { 
         "WW":
           FS.FitSetting(SC.DOUBLE_GAUSSIAN, 
-            {"f1" : 0.9,
+            {"f1" : 0.8,
             "f_m1" : 1.0,
-            "f_m2" : 0.95,
+            "f_m2" : 1.0,
             "f_s1" : 0.5,
-            "f_s2" : 2.0
+            "f_s2" : 1.5
           }), 
         "ZZ":
           FS.FitSetting(SC.DOUBLE_GAUSSIAN, 
-            {"f1" : 0.9,
+            {"f1" : 0.8,
             "f_m1" : 1.0,
-            "f_m2" : 0.8,
+            "f_m2" : 1.0,
             "f_s1" : 0.5,
-            "f_s2" : 2.0
+            "f_s2" : 1.5
           })
       },
     "jc": 
@@ -182,7 +182,7 @@ default_settings = {
       { 
         "WW":
           FS.FitSetting(SC.DOUBLE_GAUSSIAN, 
-            {"f1" : 0.9,
+            {"f1" : 0.8,
             "f_m1" : 1.0,
             "f_m2" : 1.0,
             "f_s1" : 0.5,
@@ -190,10 +190,10 @@ default_settings = {
           }), 
         "ZZ":
           FS.FitSetting(SC.DOUBLE_GAUSSIAN, 
-            {"f1" : 0.9,
+            {"f1" : 0.8,
             "f_m1" : 1.0,
-            "f_m2" : 0.8,
-            "f_s1" : 0.7,
+            "f_m2" : 1.0,
+            "f_s1" : 0.5,
             "f_s2" : 1.5
           })
       },
@@ -209,30 +209,30 @@ default_settings = {
           }), 
         "ZZ":
           FS.FitSetting(SC.DOUBLE_GAUSSIAN, 
-            {"f1" : 0.7,
-            "f_m1" : 1.1,
-            "f_m2" : 0.9,
+            {"f1" : 0.8,
+            "f_m1" : 1.0,
+            "f_m2" : 1.0,
             "f_s1" : 0.5,
-            "f_s2" : 2.0
+            "f_s2" : 1.5
           })
       },
     "gen": 
       { 
         "WW":
           FS.FitSetting(SC.DOUBLE_GAUSSIAN, 
-            {"f1" : 0.7,
+            {"f1" : 0.8,
             "f_m1" : 1.0,
-            "f_m2" : 0.98,
-            "f_s1" : 0.9,
-            "f_s2" : 1.2
+            "f_m2" : 1.0,
+            "f_s1" : 0.5,
+            "f_s2" : 1.5
           }), 
         "ZZ":
           FS.FitSetting(SC.DOUBLE_GAUSSIAN, 
-            {"f1" : 0.7,
+            {"f1" : 0.8,
             "f_m1" : 1.0,
-            "f_m2" : 0.98,
-            "f_s1" : 0.9,
-            "f_s2" : 1.2
+            "f_m2" : 1.0,
+            "f_s1" : 0.5,
+            "f_s2" : 1.5
           })
       }
     }
